b28e64326a051ea5d05566b9717ffde442362411
Catherine Meade <cat@heysparkbox.com>
2019-08-09 15:52:25 -0400

    chore: add linting

--- src/components/bio.js
+++ src/components/bio.js
@@ -5,11 +5,11 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 
-import { rhythm } from "../utils/typography"
+import { rhythm } from '../utils/typography';
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -30,13 +30,13 @@ const Bio = () => {
         }
       }
     }
-  `)
+  `);
 
-  const { author, social } = data.site.siteMetadata
+  const { author, social } = data.site.siteMetadata;
   return (
     <div
       style={{
-        display: `flex`,
+        display: 'flex',
         marginBottom: rhythm(2.5),
       }}
     >
@@ -47,22 +47,26 @@ const Bio = () => {
           marginRight: rhythm(1 / 2),
           marginBottom: 0,
           minWidth: 50,
-          borderRadius: `100%`,
+          borderRadius: '100%',
         }}
         imgStyle={{
-          borderRadius: `50%`,
+          borderRadius: '50%',
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
+        Written by
+        {' '}
+        <strong>{author}</strong>
+        {' '}
+who lives and works in San
         Francisco building useful things.
-        {` `}
+        {' '}
         <a href={`https://twitter.com/${social.twitter}`}>
           You should follow him on Twitter
         </a>
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Bio
+export default Bio;
