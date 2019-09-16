fa1df87b1ce767273347f6645e68f1b1bdfcdd91
Catherine Meade <cat@heysparkbox.com>
2019-09-16 16:39:35 -0400

    chore: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-148098105-1',
       },
     },
     'gatsby-plugin-feed',
